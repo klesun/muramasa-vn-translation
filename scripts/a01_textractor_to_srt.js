fb48ff8c7f3be04e51af22619bd6578e61372279
Artur Klesun VN Otaku <arturklesun@gmail.com>
2021-04-04 19:52:59 +0300

    Add extracted text and translations for chapter 4

--- scripts/a01_textractor_to_srt.js
+++ scripts/a01_textractor_to_srt.js
@@ -23,7 +23,7 @@ const parseObsTimestamp = (fileName) => {
     return new Date(normalized).getTime();
 };
 
-const chapterDir = __dirname + '/../public/assets/ma_common_route/ma01_crimson_craft';
+const chapterDir = __dirname + '/../public/assets/ma_common_route/ma04_sky_shaking_craft';
 
 const parseTranslatorBlock = (lineTimingText) => {
     const lines = lineTimingText.split(/(?:\r\n|\n)/);
