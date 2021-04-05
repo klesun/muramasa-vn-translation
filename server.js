e2d494b1052de6b14b3f113797dd31057fe88f8f
Artur Klesun ECMA God <arturklesun@gmail.com>
2021-04-05 20:29:50 +0300

    Get rid of node-static
    It magically adds cache-control headers with heuristic number, and did not find how to disable within 10 minutes of googling, hate it!

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 import proxyRequest from "./backend/api/proxyRequest.js";
 import streamSentences from "./backend/api/streamSentences.js";
 import addGarejeyKeyframe from "./backend/api/addGarejeyKeyframe.js";
-
-import nodeStatic from 'node-static';
+import * as url from 'url';
+import * as fsSync from 'fs';
+import * as path from 'path';
 import * as http from 'http';
+import {getMimeByExt} from 'klesun-node-tools/src/Utils/HttpUtil.js';
+import {dirname} from "path";
+import {fileURLToPath} from "url";
 
-const file = new (nodeStatic.Server)('./public');
+const fs = fsSync.promises;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const PUBLIC_PATH = path.resolve(__dirname, './public');
+
+class HttpError extends Error {
+    constructor(statusCode, message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+const serveStaticFile = async (rq, rs) => {
+    const parsedUrl = url.parse(rq.url);
+    const pathname = decodeURIComponent(parsedUrl.pathname);
+    const normalizedPathname = pathname.endsWith('/')
+        ? pathname + 'index.html'
+        : pathname;
+
+    const absPath = path.resolve(PUBLIC_PATH + '/' + normalizedPathname);
+    if (!absPath.startsWith(PUBLIC_PATH + '/') && absPath !== PUBLIC_PATH) {
+        throw new HttpError(400, 'Invalid path requested: ' + pathname);
+    }
+    if (!fsSync.existsSync(absPath)) {
+        throw new HttpError(404, 'File ' + pathname + ' does not exist');
+    }
+    const ext = absPath.replace(/^.*\./, '');
+    const mime = getMimeByExt(ext);
+    if (mime) {
+        rs.setHeader('Content-Type', mime);
+    }
+    fsSync.createReadStream(absPath).pipe(rs);
+};
 
 const handleHttpRequest = async (req, res) => {
     if (req.url === '/api/proxyRequest') {
@@ -15,7 +50,7 @@ const handleHttpRequest = async (req, res) => {
     } else if (req.url === '/api/addGarejeyKeyframe') {
         await addGarejeyKeyframe(req, res);
     } else {
-        await file.serve(req, res);
+        await serveStaticFile(req, res);
     }
 };
 
@@ -25,7 +60,7 @@ const handleHttpRequest = async (req, res) => {
  */
 const handleHttpRequestSafe = (req, res) => {
     handleHttpRequest(req, res).catch(exc => {
-        res.statusCode = 500;
+        res.statusCode = exc?.statusCode || 500;
         res.statusMessage = ((exc || {}).message || exc + '' || '(empty error)')
             // sanitize, as statusMessage seems to not allow special characters
             .slice(0, 300).replace(/[^ -~]/g, '?');
@@ -33,5 +68,8 @@ const handleHttpRequestSafe = (req, res) => {
     });
 };
 
+// const PORT = 80;
+const PORT = 19424;
+
 http.createServer(handleHttpRequestSafe)
-    .listen(80, () => console.log('Now you can open http://localhost:80 in your browser ;)'));
+    .listen(PORT, () => console.log('Now you can open http://localhost:' + PORT + '/views/untranslate.html in your browser ;)'));
